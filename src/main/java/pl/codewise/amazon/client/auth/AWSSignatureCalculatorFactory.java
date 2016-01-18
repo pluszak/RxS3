60893ba2a29b8f69ed68845bbf8d9443b94815a7
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2016-01-18 09:27:38 +0100

    Using Netty's fast thread local instead of Java's.

--- src/main/java/pl/codewise/amazon/client/auth/AWSSignatureCalculatorFactory.java
+++ src/main/java/pl/codewise/amazon/client/auth/AWSSignatureCalculatorFactory.java
@@ -1,14 +1,19 @@
 package pl.codewise.amazon.client.auth;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
+import io.netty.util.concurrent.FastThreadLocal;
 
 public class AWSSignatureCalculatorFactory {
 
-    private final ThreadLocal<AWSSignatureCalculator> signatureCalculator;
-
+    private final FastThreadLocal<AWSSignatureCalculator> signatureCalculator;
 
     public AWSSignatureCalculatorFactory(AWSCredentialsProvider credentialsProvider) {
-        signatureCalculator = ThreadLocal.withInitial(() -> new AWSSignatureCalculator(credentialsProvider));
+        signatureCalculator = new FastThreadLocal<AWSSignatureCalculator>() {
+            @Override
+            protected AWSSignatureCalculator initialValue() {
+                return new AWSSignatureCalculator(credentialsProvider);
+            }
+        };
     }
 
     public AWSSignatureCalculator getSignatureCalculator() {
